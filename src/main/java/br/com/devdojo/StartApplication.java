020a2f52376a61f6be7bfdd346ca84bf0d77634b
engdough <gilmar500@gmail.com>
2019-06-11 23:57:27 -0300

    @componente, @autowired e @Springbootapplication

--- src/main/java/br/com/devdojo/StartApplication.java
+++ src/main/java/br/com/devdojo/StartApplication.java
@@ -1,11 +1,11 @@
-package br.com.devdojo.start;
+package br.com.devdojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "br.com.devdojo.StudentEndpoint")
+@SpringBootApplication
 public class StartApplication {
     public static void main(String[] args){
         SpringApplication.run(StartApplication.class, args);
--- src/main/java/br/com/devdojo/StudentEndpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/StudentEndpoint/StudentEndpoint.java
@@ -1,10 +1,13 @@
 package br.com.devdojo.StudentEndpoint;
 
 import br.com.devdojo.model.Student;
+import br.com.devdojo.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -12,8 +15,11 @@ import static java.util.Arrays.asList;
 @RestController
 @RequestMapping("student")
 public class StudentEndpoint {
+    @Autowired
+    private DateUtil dateUtil;
     @RequestMapping(method = RequestMethod.GET, path = "/list")
     public List<Student> listAll(){
+        System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return asList(new Student("Gilmar"), new Student("Douglas"));
     }
 }
